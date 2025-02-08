50e4987094efdedd33e1b6f51ff372d889fe87f2
Ngoc-Khanh <dongockhanh2003@gmail.com>
2025-02-09 03:14:54 +0700

    update mongodb connect

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,9 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements JWTSubject
+class User extends UserAuthModel implements JWTSubject
 {
     use HasFactory, Notifiable;
 
